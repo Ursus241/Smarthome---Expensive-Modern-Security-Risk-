502ac9fc2070d9aa3229ddd820063864330b26b9
Ursus241 <74021033+Ursus241@users.noreply.github.com>
2020-12-21 15:12:59 +0100

    Create backup-image.sh

---
+++ Raspberry Installation/Help/backup-image.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+#Verwendung: 
+sudo ./backup_image.sh
+#erzeugt ein Image im Verzeichnis /media/usb
+#Eine Vorraussetzung ist das Mounten eines USB-Sticks oder einer externen Festplatte in diesem Verzeichnis
